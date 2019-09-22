c7b6f14595590c3a8f1727c738e78da27139e45d
Abigayle Franklin <abifanklin@gmail.com>
2019-09-22 09:39:16 -0500

    Photo uploading added

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const userRouter = require('./config/user')
 const listRouter = require('./config/list')
 const itemRouter = require('./config/item')
 
+//For Picture Uploading
+const cloudinary = require('cloudinary')
+const formData = require('express-form-data')
+const cors = require('cors')
+const { CLIENT_ORIGIN } = require('./config')
+
 //Cookie middleware
 server.use(session({
   name: 'session',
@@ -19,6 +25,15 @@ server.use(session({
     expires: new Date(Date.now() + 2 * 60 * 60 * 1000) // 2 hour
   }
 }))
+
+//Picture Uploading
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET
+})
+
+
 
 //Auth middleware
 const auth = (req,res,next) => {
@@ -41,4 +56,39 @@ server.get('/', auth, (req, res) => {
   res.status(200).send({message: 'I am alive'})
 })
 
+server.use(cors({ 
+  origin: CLIENT_ORIGIN 
+})) 
+
+server.use(formData.parse())
+
+server.get('/wake-up', (req, res) => res.send('👌'))
+
+server.post('/image-upload', (req, res) => {
+
+  const values = Object.values(req.files)
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+  
+  Promise
+    .all(promises)
+    .then(results => res.json(results))
+    .catch((err) => res.status(400).json(err))
+})
+
 server.listen(process.env.PORT, ()=>console.log(`Server started at port ${process.env.PORT}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
