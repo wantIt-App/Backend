418b4a910530a06dcd654995d40a1949c0b1f115
WilliamConnatser <43946230+WilliamConnatser@users.noreply.github.com>
2019-09-28 23:51:42 -0500

    fixed bug in login endpoint- improved error logging- finished invites router

--- apis/db/invite.js
+++ apis/db/invite.js
@@ -1,18 +1,23 @@
 const db = require('./global')
 
+const getOne = (filter) => {
+    return db('invites').where(filter).first()
+}
+
 const getMany = (filter) => {
     return db('invites').where(filter)
 }
 
 const insert = (data) => {
-    return db('invites').insert(data)
+    return db('invites').insert(data).returning(["list_id", "invitee_id", "rejected"])
 }
 
 const update = (filter, data) => {
-    return db('invites').where(filter).update(data)
+    return db('invites').where(filter).update(data).returning(["list_id", "invitee_id", "rejected"])
 }
 
 module.exports = {
+    getOne,
     getMany,
     insert,
     update
--- config/invite.js
+++ config/invite.js
@@ -21,15 +21,25 @@ inviteRouter.post('/', async (req, res) => {
         const applicableList = await listApi.getOne({ id: input.list_id })
         //Make sure the user inviting other users owns the list
         if (applicableList.owner_id === req.user.id) {
+            console.log({
+                list_id: input.list_id,
+                invitee_id: input.invitee_id
+            })
             inviteApi.insert({
                 list_id: input.list_id,
                 invitee_id: input.invitee_id
             })
                 .then((data) => {
+                    console.log(data)
                     res.status(200).send({ data: { invite: data[0] } })
                 })
-                .catch((data) => {
-                    res.status(500).send({message: "Internal Server Error"})
+                .catch((err) => {
+                    if (err.message.includes('unique constraint')) {
+                        res.status(200).send({ message: 'This user has already been invited to this list' })
+                    } else {
+                        console.log(err.message, 'Error @ POST /invite')
+                        res.status(500).send({message: "Internal Server Error"})
+                    }
                 })
         } else {
             res.status(422).send({ message: "You don't own this list" })
@@ -43,27 +53,30 @@ inviteRouter.put('/toggle', async (req, res) => {
     //Validate Input
     if (!input) {
         res.status(422).send({ message: "No invite information was provided in the request" })
-    } else if (input.invite_id === undefined) {
-        res.status(422).send({ message: "No invite id was provided in the request" })
-    } else if (!Number.isInteger(input.invite_id)) {
-        res.status(422).send({ message: "Invite id must be an integer" })
+    } else if (input.list_id === undefined) {
+        res.status(422).send({ message: "No list id was provided in the request" })
+    } else if (!Number.isInteger(input.list_id)) {
+        res.status(422).send({ message: "List id must be an integer" })
     } else {
-        const applicableInvite = await inviteApi.getOne({ id: input.invite_id })
-        //Make sure the user inviting other users owns the list
-        if (applicableInvite.invitee_id === req.user.id) {
+        //Include user's ID to make sure the user is the invitee
+        const applicableInvite = await inviteApi.getOne({ list_id: input.list_id, invitee_id: req.user.id })
+        if (applicableInvite) {
             inviteApi.update({
-                id: input.invite_id
+                list_id: input.list_id,
+                invitee_id: req.user.id
             }, {
+                //Toggle the rejected state
                 rejected: !applicableInvite.rejected
             })
                 .then((data) => {
                     res.status(200).send({ data: { invite: data[0] } })
                 })
-                .catch((data) => {
+                .catch((err) => {
+                    console.log(err.message, 'Error @ PUT /invite/toggle')
                     res.status(500).send({ message: "Internal Server Error" })
                 })
         } else {
-            res.status(422).send({ message: "You are not authorize to reject or accept this invite" })
+            res.status(422).send({ message: "This invite does not belong to you" })
         }
     }
 })
